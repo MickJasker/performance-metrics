aa94a81ee5d76b4ba20ffbf0fd57288c24627711
Mick Jasker <mick.jasker@mediamonks.com>
2019-04-02 14:23:23 +0200

    Release 1.0.0

--- lib/lib/PerformanceMetrics.d.ts
+++ lib/lib/PerformanceMetrics.d.ts
@@ -1,8 +1,10 @@
-import IDeviceSpecifications from './IDeviceSpecifications';
-import INetworkBandwidthInformation from "./INetworkBandwidthInformation";
+import IDeviceSpecifications from './interfaces/IDeviceSpecifications';
+import INetworkBandwidthInformation from "./interfaces/INetworkBandwidthInformation";
+import IFPSCounter from './interfaces/IFPSCounter';
 declare class PerformanceMetrics {
     deviceSpecifications: IDeviceSpecifications;
     networkBandwidthInformation: INetworkBandwidthInformation;
+    fpsCounter: IFPSCounter;
     constructor();
 }
 export default PerformanceMetrics;
