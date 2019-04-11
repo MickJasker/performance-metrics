27f699960b4028c6696ffd989b3f69214bea0398
Mick Jasker <mick.jasker@mediamonks.com>
2019-04-11 13:50:59 +0200

    Changed build setup

---
+++ lib/DeviceSpecifications.d.ts
@@ -0,0 +1,72 @@
+interface IBrowser {
+    name: string;
+    version: string;
+    major: string;
+}
+interface IOS {
+    name: string;
+    version: string;
+    major: string;
+}
+interface IDevice {
+    name: string;
+}
+/**
+ * @namespace DeviceSpecifications
+ * @class DeviceSpecifications
+ * @constructor
+ * @param {string} UserAgentString Provide a user agent, if none is provided `navigator.useragent` will be used by default.
+ * @param {string[]} browserslist Provide a `string[]` of a browserslist to manually overide the default value: `['last 5 versions', '> 3%', 'not ie <= 11']`
+ */
+declare class DeviceSpecifications {
+    private _agent;
+    /** Returns the used useragent */
+    readonly userAgentString: string;
+    private readonly _browsersList;
+    /**
+     * @constructor
+     * @param {string} UserAgentString Provide a user agent, if none is provided `navigator.useragent` will be used by default.
+     * @param {string[]} browserslist Provide a `string[]` of a browserslist to manually overide the default value: `['last 5 versions', '> 3%', 'not ie <= 11']`
+     */
+    constructor(UserAgentString?: string, browserslist?: string[]);
+    /**
+     * Returns returns an object containing:
+     * -  name
+     * -  version (eg. 1.2.3)
+     * -  major version
+     *
+     * @method getBrowser
+     * @return {IBrowser}
+     */
+    getBrowser(): IBrowser;
+    /**
+     *    * Returns returns an object containing:
+     * -  name
+     * -  version (eg. 1.2.3)
+     * -  major version
+     *
+     * @method getOperatingSystem
+     * @return {IOS}
+     */
+    getOperatingSystem(): IOS;
+    /**
+     * Returns the mobile device typs (eg. iPhone, Android, etc.)
+     *
+     * Only works on mobile devices!
+     *
+     * @method getDevice
+     * @return {IDevice}
+     */
+    getDevice(): IDevice;
+    /**
+     * Returns if the browser is up to date using browserslist.
+     * Keep the library up-to-date to keep this functionality working properly
+     *
+     * Warning: this is general detection if the browser is a modern browser, it is not a replacement of feature detection libraries like mordernizer.
+     *
+     * @method isBrowserModern
+     * @return {boolean}
+     */
+    isBrowserModern(): boolean;
+}
+export default DeviceSpecifications;
---
+++ lib/NetworkBandwidthInformation.d.ts
@@ -0,0 +1,29 @@
+/**
+ * @namespace NetworkBandwidthInformation
+ * @class NetworkBandwidthInformation
+ * @constructor
+ * @param {number} minimumFileSize Provide the minimum file size of the files you want to get the bandwidth of.
+ */
+declare class NetworkBandwidthInformation {
+    /** `number[]` containing all bandwidths. */
+    bandwidths: number[];
+    /** Average of `bandwidths` property. */
+    averageBandwidth: number;
+    private _minSize;
+    /**
+     * @constructor
+     * @param {number} minimumFileSize Provide the minimum file size of the files you want to get the bandwidth of.
+     */
+    constructor(minimumFileSize?: number);
+    /**
+     * Returns an array of all bandwidths.
+     * @method getBandwidths
+     */
+    getBandwidths(): number[];
+    /**
+     * Returns an average of `this.bandwidths`
+     * @method getAverageBandwidth
+     */
+    getAverageBandwidth(): number;
+}
+export default NetworkBandwidthInformation;
